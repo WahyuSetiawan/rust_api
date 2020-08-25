940f470f52725589077c2b4d0ccdab3f17175a38
kijang <wahyu.creator911@gmail.com>
2020-08-25 12:10:39 +0700

    udpate .env

---
+++ migrations/V1__create_employee.sql
@@ -0,0 +1,6 @@
+create table if not exists employee { 
+    id int PRIMARY_KEY AUTO_INCREMENT,
+    firstname varchar(30) not null,
+    lastname varchar(30) not null
+    email varchar(50)
+}
--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,12 @@
 extern crate rocket;
 #[macro_use]
 extern crate serde_derive;
+extern crate envfile;
 
 use mysql::*;
+use envfile::EnvFile;
+use std::io;
+use std::path::Path;
 
 mod database;
 mod services;
@@ -29,11 +33,21 @@ fn main() {
     //     )
     //     .launch();
 
-    let url = "mysql://root:08111993@localhost:3306/testing";
+    // let url = "mysql://root:08111993@localhost:3306/testing";
 
-    let pool = Pool::new(url).unwrap();
+    // let pool = Pool::new(url).unwrap();
 
-    let mut conn = pool.get_conn().unwrap();
+    // let mut conn = pool.get_conn().unwrap();
 
-    embedded::migrations::runner().run(&mut conn).unwrap();
+    // embedded::migrations::runner().run(&mut conn).unwrap();
+
+    let mut envfile = EnvFile::new(&Path::new(".env")).unwrap();
+
+
+    for (key, value) in &envfile.store {
+        println!("{}: {}", key, value);
+    }
+
+    envfile.update("ID", "example");
+    println!("ID: {}", envfile.get("ID").unwrap_or(""));
 }
