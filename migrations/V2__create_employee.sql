379d1c7097ad04d516390022ee1c7c0fb47b3c03
kijang <wahyu.creator911@gmail.com>
2020-08-25 13:50:23 +0700

    update migrate remove migrations

--- migrations/V2__create_employee.sql
+++
@@ -1,6 +0,0 @@
-create table if not exists employee { 
-    id int PRIMARY_KEY AUTO_INCREMENT,
-    firstname varchar(30) not null,
-    lastname varchar(30) not null
-    email varchar(50)
-}
--- migrations/V3__create_employee.sql
+++
@@ -1,8 +0,0 @@
-create table employee (
-    id int PRIMARY KEY AUTO INCREMENTS,
-    first_name varchar(25) not null,
-    last_name varchar(25) not null,
-    birth_date DATE,
-    hire_date DATE,
-    gander ENUM('M', 'F')
-)
